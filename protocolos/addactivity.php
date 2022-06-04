9147b026e5d3978e395d3b374d022ba3e09025ed
David <torren.david59@gmail.com>
2022-06-03 21:44:22 -0500

    hello

--- protocolos/addactivity.php
+++ protocolos/addactivity.php
@@ -1,15 +1,28 @@
 <?php
+session_start();
 if(isset($_POST["op"])){
-    $actividadid=123;
     $nombre=$_POST["nombre"];
     $descripcion=$_POST["descripcion"];
     if($nombre==null || $descripcion==null){
         header('Location:http://localhost/FinalPHP/vistas/agregaractividad.php');
-        session_start();
+        
         $_SESSION["ActivityState"]="Completa todos los campos para crear esta actividad";
     }
+    else if(strlen($nombre)>15){ 
+            header('Location:http://localhost/FinalPHP/vistas/agregaractividad.php');
+            $_SESSION["ActivityState"]="El nombre debe tener menos de 15 caracteres";
+        }
+    elseif(strlen($descripcion)>100){
+            header('Location:http://localhost/FinalPHP/vistas/agregaractividad.php');
+            $_SESSION["ActivityState"]="La descripción debe tener menos de 100 caracteres";
+        }
+    
     else{
         include("../conexion/abrir_conexion.php");
+        $paso=0;
+        while($paso==0){
+            //SEGUIR AQUÍ, VALIDAR QUE NO EXISTE EL ID
+        }
 
     $CREATEACT="INSERT INTO $actividad 
     (actividadid,nombre,descripcion)
@@ -18,7 +31,6 @@ if(isset($_POST["op"])){
 
     mysqli_query($conexion,$CREATEACT);
     header('Location:http://localhost/FinalPHP/vistas/agregaractividad.php');
-    session_start();
     $_SESSION["ActivityState"]="Actividad guardada exitosamente";
         include("../conexion/cerrar_conexion.php");
     }
